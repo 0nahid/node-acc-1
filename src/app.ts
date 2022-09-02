3662c04063ba9217ccb2b93a86a1db9095aeb103
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-03 03:59:43 +0600

    removed html file

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,19 @@ import { default as userRouter } from "./routes/v1/users.route";
 
 /* here will be the all the routes */
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(__dirname + "/index.html");
+  res.send(`
+  <h1>Welcome to the User API</h1>
+  <p>Here are the available routes</p>
+  <ul>
+    <li><a href="/">/home</a></li>
+    <li><a href="/user/random">/user/random</a></li>
+    <li><a href="/user/1">/user/:id</a></li>
+    <li><a href="/user/all">/user/all</a></li>
+    <li><a href="/user/save">/user/save</a></li>
+    <li><a href="/user/patch">/user/patch</a></li>
+    <li><a href="/user/patch/bulk-update">/user/patch/bulk-update</a></li>
+    <li><a href="/user/:id">/user/delete</a></li>
+  </ul>`);
 });
 
 /* Here is the User Routes */
